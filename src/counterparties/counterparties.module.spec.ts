96b62891836413add3bca1beba8bb7d7d6ca3f5f
B-Dmitriy <balyaevds.main@gmail.com>
2024-02-11 12:09:50 +0300

    feat: finish counterparty module

--- src/counterparties/counterparties.module.spec.ts
+++ src/counterparties/counterparties.module.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CounterpartiesController } from './counterparties.controller';
 import { CounterpartiesService } from './counterparties.service';
 
-describe('CounterpartiesService', () => {
+describe('CounterpartiesController', () => {
+  let controller: CounterpartiesController;
   let service: CounterpartiesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [CounterpartiesController],
       providers: [CounterpartiesService],
     }).compile();
 
+    controller = module.get<CounterpartiesController>(CounterpartiesController);
     service = module.get<CounterpartiesService>(CounterpartiesService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 
   it('should be defined', () => {
--- src/counterparties/interfaces/counterparty.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Counterparty {
-  id: number;
-  name: string;
-  description: string;
-  createdAt: string;
-  updatedAt: string;
-}
